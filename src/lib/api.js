dd9481953b4e413f393d971bbe43cda526616e96
bruno918 <bruno-leal@educar.rs.gov.br>
2022-10-30 17:35:39 -0300

    fix ytdlmp4

--- src/lib/api.js
+++ src/lib/api.js
@@ -116,7 +116,45 @@ async function ytPlayMp3(query) {
         console.log(error)
     })
 }
+//YTMP4
+async function ytDonlodMp4(url) {
+  return new Promise((resolve, reject) => {
+    try {
+      const id = yt.getVideoID(url)
+      const yutub = yt.getInfo(`https://www.youtube.com/watch?v=${id}`)
+      .then((data) => {
+        let pormat = data.formats
+        let video = []
+        for (let i = 0; i < pormat.length; i++) {
+          if (pormat[i].container == 'mp4' && pormat[i].hasVideo == true && pormat[i].hasAudio == true) {
+            let vid = pormat[i]
+            video.push(vid.url)
+          }
+        }
+        const title = data.player_response.microformat.playerMicroformatRenderer.title.simpleText
+        const thumb = data.player_response.microformat.playerMicroformatRenderer.thumbnail.thumbnails[0].url
+        const channel = data.player_response.microformat.playerMicroformatRenderer.ownerChannelName
+        const views = data.player_response.microformat.playerMicroformatRenderer.viewCount
+        const published = data.player_response.microformat.playerMicroformatRenderer.publishDate
+        
+        const result = {
+          título: title,
+          thumb: thumb,
+          canal: channel,
+          publicado: published,
+          visualizações: views,
+          link: video[0]
+        }
+        return(result)
+      })
+      resolve(yutub)
+    } catch (error) {
+        reject(error);
+      }
+      console.log(error)
+  })
+}
 
 
-module.exports = {  ytDonlodMp3, ytPlayMp3, TelegraPh }
+module.exports = {  ytDonlodMp3, ytDonlodMp4, ytPlayMp3, TelegraPh }
 
